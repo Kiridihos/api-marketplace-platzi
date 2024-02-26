540f2e709aa9b7442ac75781108e93c0557f92e3
Kiridihos <diego.alexander.fino@gmail.com>
2024-02-26 12:21:37 -0500

    Se implementa swagger para la documentacion del api y trabaja mediante al url swagger-ui/index.html

---
+++ src/main/java/com/example/demo/web/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.web.config;
+
+
+//@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select().apis(RequestHandlerSelectors.basePackage("com.example.demo.web.controller"))
+//                .build();
+//    }
+}
